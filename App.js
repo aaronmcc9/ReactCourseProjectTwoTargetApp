2aa55e7fd5d8b46e4ccd8463e0e057c3069a891f
Aaron McCarthy <Aaron.McCarthy@logibec.com>
2023-12-19 19:05:15 -0800

    added gradient and background image

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient'
 
 
 
 
 export default function App() {
   return (
-    <StartGameScreen></StartGameScreen>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground resizeMode='cover'
+        style={styles.rootScreen}
+        source={require("./assets/images/background.png")}
+        imageStyle={styles.backgroundImage}>
+        <StartGameScreen></StartGameScreen>
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-
+  rootScreen: {
+    // backgroundColor: "#ddb2f",
+    flex: 1
+  },
+  backgroundImage: {
+    opacity: 0.75
+  }
 });
